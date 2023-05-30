8267cc2d2339ca4b8e3d1cadf822419e0a7b14ba
Yoonjae Choi <dbswo9795@gmail.com>
2023-05-31 01:12:37 +0900

    [Feat] 회원가입 틀 구현

--- src/pages/userPage/style.ts
+++ src/pages/userPage/style.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import {NavLink} from "react-router-dom";
 
+// 로그인 css
+
 export const LoginSection = styled.div`
   position: absolute;
   left: 68.5rem;
@@ -84,4 +86,88 @@ export const SignUpButton = styled(NavLink)`
   font-size: 1.6rem;
   font-weight: 300;
   text-decoration: none;
+`
+
+// 회원가입 css
+
+export const SignUpSection = styled.div`
+  position: absolute;
+  left: 68.5rem;
+  right: 68.5rem;
+  margin: 17rem 0 26.5rem;
+  width: 55rem;
+  height: 77rem;
+  background-color: #F7F8F9;
+  border-radius: 20px;
+  filter: drop-shadow(0 4px 4px rgb(0, 0, 0, 25%));
+`
+
+export const SignUpForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 4.3rem;
+`
+
+export const InputContainer = styled.div`
+  width: 45rem;
+`
+
+export const EmailContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 45rem;
+  height: 5.6rem;
+  background-color: #FFFFFF;
+  border: 1px solid #CCCCCC;
+  border-radius: 8px;
+  box-sizing: border-box;
+`
+
+export const DataName = styled.p`
+  margin-bottom: 0.8rem;
+  text-align: start;
+  font-size: 1.2rem;
+  color: #666666;
+`
+
+export const EmailData = styled.input`
+  padding-left: 1rem;
+  border: none;
+  outline: none;
+  text-align: start;
+  font-size: 1.6rem;
+  color: #666666;
+`
+
+export const EmailButton = styled.button`
+  width: 7.5rem;
+  height: 3.5rem;
+  margin-right: 1.5rem;
+  background-color: var(--button--color);
+  border: none;
+  border-radius: 5px;
+  color: #FFFFFF;
+  font-weight: 100;
+  font-size: 1.2rem;
+  cursor: pointer;
+`
+
+export const DataInput = styled.input`
+  width: 45rem;
+  height: 5.6rem;
+  padding-left: 1rem;
+  border: 1px solid #CCCCCC;
+  outline: none;
+  border-radius: 8px;
+  box-sizing: border-box;
+  font-size: 1.6rem;
+`
+
+export const CheckValue = styled.p`
+  text-align: start;
+  margin: 0.2rem 0.3rem 0;
+  color: #EB5757;
+  font-size: 1.2rem;
 `
