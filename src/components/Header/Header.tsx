4ba7816e42478e045ac958b0852fcdeb43f2c7fb
Yoonjae Choi <dbswo9795@gmail.com>
2023-05-30 16:14:09 +0900

    [Fix] NavLink 구현 Logo아이콘 클릭 시 메인페이지로 이동 로그인 버튼 클릭 시 로그인 페이지로 이동

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,22 @@
 import React from "react";
-import {NavLink} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {HeaderSection, HeaderContainer, LogoContainer, MainLogo, NavContainer, NavCategory, LoginButton, SearchButton, SearchIcon} from "./style";
 
 import mainLogo from "../../assets/icons/mainlogo.svg";
 import searchLogo from "../../assets/icons/search.svg";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  // 로그인 버튼을 클릭하여 로그인 화면으로 이동
+  const loginHandler = () => {
+    navigate("/login");
+  }
+
   return (
     <HeaderSection>
       <HeaderContainer>
-        <LogoContainer>
+        <LogoContainer to="/">
           <MainLogo src={mainLogo} alt="mainLogo" />
         </LogoContainer>
         <NavContainer>
@@ -17,7 +24,7 @@ const Header = () => {
           <NavCategory>커뮤니티</NavCategory>
           <NavCategory>봉사후기</NavCategory>
         </NavContainer>
-        <LoginButton>로그인</LoginButton>
+        <LoginButton onClick={loginHandler}>로그인</LoginButton>
         <SearchButton>
           <SearchIcon src={searchLogo} alt="searchLogo" />
         </SearchButton>
