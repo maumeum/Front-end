c06da6dd1c3908d45e749d9e09544fc9121be144
Yoonjae Choi <dbswo9795@gmail.com>
2023-05-30 12:56:09 +0900

    [Feat] react-router-dom으로 라우터 환경 구축 Router, Routes, Route로 분리 MainPage Router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 import Header from './components/Header/Header';
+import Main from "./pages/mainPage/main";
 
 function App() {
 
   return (
-    <div>
-      <Header />      
-      <h1>마음이음</h1>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>    
+    </Router>
   )
 }
 
