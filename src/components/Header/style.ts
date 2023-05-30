fa3d45508bfd7248bebabb052fd769d448c2365b
Yoonjae Choi <dbswo9795@gmail.com>
2023-05-30 16:13:07 +0900

    [Style] 로그인 페이지 css 구현, 메인 페이지 수정 로그인 페이지 css 적용 메인 페이지 css 수정 : 로그인 페이지 이동 NavLink, Logo NavLink index.css: body에만 폰트 적용 대신 모든 DOM에 폰트 적용 App.css: backgroud-color 제거

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {NavLink} from "react-router-dom";
 
 export const HeaderSection = styled.div`
   position: sticky;
@@ -17,7 +18,7 @@ export const HeaderContainer = styled.div`
   
 `;
 
-export const LogoContainer = styled.div`
+export const LogoContainer = styled(NavLink)`
   width: 8rem;
   height: 8rem;
 `
@@ -46,8 +47,9 @@ export const LoginButton = styled.button`
   background-color: transparent;
   border-radius: 1.7rem;
   border: 1px solid #CCCCCC;
-  font-size: 1.1rem;
-  font-weight: 500;
+  text-align: center;
+  font-size: 1.2rem;
+  font-weight: 400;
   cursor: pointer;
 `
 
---
+++ src/pages/userPage/style.ts
@@ -0,0 +1,86 @@
+import styled from "styled-components";
+import {NavLink} from "react-router-dom";
+
+export const LoginSection = styled.div`
+  position: absolute;
+  left: 68.5rem;
+  right: 68.5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 8rem;
+  width: 55rem;
+  height: 75rem;
+`
+
+export const LogoContainer = styled.div`
+  margin: 5rem 0 1.5rem;
+  width: 12rem;
+  height: 12rem;
+`
+
+export const LogoImage = styled.img`
+  width: 12rem;
+  height: 12rem;
+`
+
+export const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 55rem;
+  height: 55rem;
+  border: 1px solid #D9D9D9;
+`
+
+export const EmailInput = styled.input`
+  margin-top: 7rem;
+  width: 45rem;
+  height: 6rem;
+  border: none;
+  outline: none;
+  border-bottom: 2px solid #ccc;
+  font-size: 2rem;
+  font-weight: bold;
+`
+
+export const PasswordInput = styled.input`
+  width: 45rem;
+  height: 6rem;
+  border: none;
+  outline: none;
+  border-bottom: 2px solid #ccc;
+  font-size: 2rem;
+  font-weight: bold;
+`
+export const CheckEmail = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 3rem;
+  width: 45rem;
+  height: 6rem;
+  background-color: #fafafa;
+  color: #EB5757;
+  font-size: 1.3rem;
+`
+
+export const LoginButton = styled.button`
+  margin-top: 7rem;
+  width: 45rem;
+  height: 5.5rem;
+  background-color: var(--button--color);
+  color: #FFFFFF;
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
+`
+
+export const SignUpButton = styled(NavLink)`
+  align-self: flex-start;
+  margin: 13rem 0 3rem 5rem;
+  color: #888888;
+  font-size: 1.6rem;
+  font-weight: 300;
+  text-decoration: none;
+`
