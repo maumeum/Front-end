c4dc6a8070613a301c12ed25199917ed182131b4
Takkyou <takelucky777@naver.com>
2023-05-30 14:24:36 +0900

    Feat

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
-import * as React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
+import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
-import Header from './components/Header/Header';
+import Header from "./components/Header/Header";
 import Main from "./pages/mainPage/main";
 
-function App() {
+import Remove from "./pages/myPage/removePage/Remove";
 
+function App() {
   return (
     <Router>
       <Header />
       <Routes>
         <Route path="/" element={<Main />} />
-      </Routes>    
+        <Route path="remove" element={<Remove />} />
+      </Routes>
     </Router>
-  )
+  );
 }
 
 export default App;
