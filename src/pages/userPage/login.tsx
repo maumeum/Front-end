8149314ece0ea50087784907d5e8353b82f627d4
Yoonjae Choi <dbswo9795@gmail.com>
2023-05-31 01:12:19 +0900

    [Fix] useState 타입 수정

--- src/pages/userPage/login.tsx
+++ src/pages/userPage/login.tsx
@@ -1,13 +1,14 @@
 import React, {useState} from "react";
-import {LoginSection, LogoContainer, LogoImage, LoginForm, EmailInput, PasswordInput, CheckEmail, CheckData, LoginButton, SignUpButton} from "./style"
+import {LoginSection, LogoContainer, LogoImage, LoginForm, EmailInput, PasswordInput, CheckEmail, CheckData, SignUpButton} from "./style"
+import LargeButton from "../../components/Buttons/LargeButton";
 
 import mainLogo from "../../assets/icons/mainlogo.svg";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [checkEmail, setCheckEmail] = useState(true);
-  const [checkData, setCheckData] = useState(true);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [checkEmail, setCheckEmail] = useState<boolean>(true);
+  const [checkData, setCheckData] = useState<boolean>(true);
 
   //이메일 value
   const emailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -43,7 +44,7 @@ const Login = () => {
         <EmailInput placeholder="이메일" value={email} onChange={emailChange}/>
         <PasswordInput type="password" placeholder="비밀번호" value={password} onChange={passwordChange}/>
         {!checkData ? <CheckData>이메일과 비밀번호를 모두 입력해주세요.</CheckData> : !checkEmail && <CheckEmail>이메일 형식에 맞춰 입력해주세요.</CheckEmail>}
-        <LoginButton type="submit">로그인</LoginButton>
+        <LargeButton type="submit">로그인</LargeButton>
         <SignUpButton to="/sign_up">회원가입</SignUpButton>
       </LoginForm> 
     </LoginSection>
