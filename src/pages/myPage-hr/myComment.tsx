2b9515b359c4d0187f83a9cbcaf37f109152f71c
[SW4/1반] 정혜린 <flsgp0317@gmail.com>
2023-06-01 16:25:27 +0900

    [Feat] 내가 쓴 리뷰 페이지 레이아웃 완성

--- src/pages/myPage-hr/myComment.tsx
+++ src/pages/myPage-hr/myComment.tsx
@@ -8,8 +8,27 @@ import {
 import Tab from '../../components/Tab/Tab.tsx';
 import MyPost from '../../components/MyPost/MyPost.tsx';
 
+const data = [
+	{
+		title:
+			'한강 플로깅 같이 하실분 구합니다~ 카카오톡 오픈채팅방으로 들어오세요',
+		content:
+			'7월 8일 8시쯤 저녁먹고 산책할겸 한강나루역 부근에서 플로깅하실 분.생각잇으시면댓 주세요 7월 8일 8시쯤 저녁먹고 산책할겸 한강나루역 부근에서 플로깅하실 분.생각잇으시면댓 주세요 7월 8일 8시쯤 저녁먹고 산책할겸 한강나루역 부근에서 플로깅하실 분.생각잇으시면댓 주세요7월 8일 8시쯤 저녁먹고 산책할겸 한강나루역 부근에서 플로깅하실 분.생각잇으시면댓 주세요7월 8일 8시쯤 저녁먹고 산책할겸 한강나루역 부근에서 플로깅하실 분.생각잇으시면댓 주세요7월 8일 8시쯤 저녁먹고 산책할겸 한강나루역 부근에서 플로깅하실 분.생각잇으시면댓 주세요7월 8일 8시쯤 저녁먹고 산책할겸 한강나루역 부근에서 플로깅하실 분.생각잇으시면댓 주세요',
+		category: '동행구해요',
+		date: '2022-01-01',
+	},
+	{
+		title:
+			'유기견 봉사활동 고등학생때 가봤는데 냄새때문에 너무 힘들었어요.. 혹시 견사청소쪽 말고 다른 활동 위주인 단체있을가요',
+		content:
+			'지역은 충남 천안근처였으면 좋겠습니다.! 아니면 여기서 자차타고 한시간 거리도 좋아요! 댓 주세용',
+		category: '동행구해요',
+		date: '2022-01-01',
+	},
+];
+
 function myComment() {
-	const [currTab, setCurrTab] = useState('내가 댓글 단 게시글');
+	const [currTab, setCurrTab] = useState('내가 쓴 게시글');
 	const handleClickTab = (tab: string) => {
 		setCurrTab(tab);
 	};
@@ -28,7 +47,17 @@ function myComment() {
 					<TabMenu>
 						<Tab currTab={currTab} onClick={handleClickTab} tabs={tabs} />
 					</TabMenu>
-					<MyPost currTab={currTab}></MyPost>
+					{data.map((item, index) => {
+						return (
+							<MyPost
+								key={`postList-${index}`}
+								currTab={currTab}
+								title={item.title}
+								content={item.content}
+								category={item.category}
+								date={item.date}></MyPost>
+						);
+					})}
 				</Main>
 			</Container>
 		</>
