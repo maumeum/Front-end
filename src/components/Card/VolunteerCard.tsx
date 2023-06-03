374db4b0b305e7b4073d8f4e0defd2b8ba6fb88c
Yoonjae Choi <dbswo9795@gmail.com>
2023-06-03 17:02:37 +0900

    [Refactor] styled-component 파일 위치 변경

--- src/components/Card/VolunteerCard.tsx
+++ src/components/Card/VolunteerCard.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import styled from "styled-components";
+
+
+import {VolunteerSection, 
+  VolunteerImgContainer,
+  VolunteerImage, 
+  RecruitStatus, 
+  VolunteerIntroContainer, 
+  VolunteerTitle, 
+  VolunteerContent} from "./card.ts"
 
 interface listType {
   title : string,
@@ -17,70 +25,28 @@ interface listType {
   updateAt : string
 }
 
+interface VolunteerCardProps {
+  data: listType
+}
+
 
 import imgData from "../../assets/images/volunteer1.jpg";
 
-const VolunteerCard = ({data}: {data: listType}) => {
+const VolunteerCard = ({data}: VolunteerCardProps) => {
 
   return (
-    <Volunteer>
-      <ImgContainer>
-        <Image src={imgData} />
+    <VolunteerSection>
+      <VolunteerImgContainer>
+        <VolunteerImage src={imgData} />
         <RecruitStatus>{data.statusName}</RecruitStatus>
-      </ImgContainer>
-      <IntroContainer>
-        <Title>{data.title}</Title>
-        <Content>{data.centName}</Content>
-      </IntroContainer>
-    </Volunteer>
+      </VolunteerImgContainer>
+      <VolunteerIntroContainer>
+        <VolunteerTitle>{data.title}</VolunteerTitle>
+        <VolunteerContent>{data.centName}</VolunteerContent>
+      </VolunteerIntroContainer>
+    </VolunteerSection>
   )
 }
 
 export default VolunteerCard;
 
-const Volunteer = styled.div`
-  margin-bottom: 6rem;
-  width: calc(100% - 0.5rem);
-  height: 37rem;
-  background-color: #FFFFFF;
-  border: none;
-  border-radius: 12px;
-  filter: drop-shadow(0 2px 10px rgb(0, 0, 0, 10%));
-  cursor: pointer;
-`
-
-const ImgContainer = styled.div`
-  margin-bottom: 3rem;
-`
-
-const Image = styled.img`
-  width: 100%;
-  aspect-ratio: 4/3;
-  border: none;
-  border-radius: 12px;
-`
-
-const RecruitStatus = styled.div`
-  width: 25%;
-  text-align: center;
-  background-color: #202020;
-  color: #FFFFFF;
-  font-size: 1.2rem;
-  border: none;
-  border-radius: 0 4px 4px 4px;
-`
-
-const IntroContainer = styled.div`
-  margin: 1.5rem;
-`
-
-const Title = styled.h2`
-  margin-bottom: 1.5rem;
-  font-size: 1.7rem;
-  font-weight: 800;
-`
-
-const Content = styled.p`
-  margin-top: 0;
-  font-size: 1.3rem;
-`
