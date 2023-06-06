aaa74c29e915cb6e11955e028766f1239220294f
Yoonjae Choi <dbswo9795@gmail.com>
2023-06-06 21:26:01 +0900

    [Fix] search 페이지 구현

--- src/pages/searchPage/search.tsx
+++ src/pages/searchPage/search.tsx
@@ -1,21 +1,97 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { SearchSection, SearchContainer, SearchLogo, SearchInput, VolunteerContainer, CommunityContainer } from './style';
-import searchLogo from '@src/assets/icons/search.svg';
+import SearchBar from '@components/SearchBar/SearchBar';
+import { get } from '@api/Api';
+import { communityListType, volunteerListType } from '@src/types/cardType';
+import {
+	SearchSection,
+	VolunteerContainer,
+	VolunteerTitle,
+	CommunityContainer,
+	CommunityTitle,
+	NoSearchContainer,
+	NoKeyword,
+	KeywordContainer,
+	KeywordBox,
+} from './style';
+import VolunteerCard from '@components/Card/VolunteerCard';
+import CommunityCard from '@components/Card/CommunityCard';
 
 const Search = () => {
+	const [query, setQuery] = useState<string>('');
+	const [volunteerList, setVolunteerList] = useState<volunteerListType[]>([]);
+	const [communityList, setCommunityList] = useState<communityListType[]>([]);
+
+	const handleSearch = (query: string) => {
+		setQuery(query);
+	};
+
+	// 봉사활동 조회
+	useEffect(() => {
+		const fetchData = async () => {
+			const responseData = await get<volunteerListType[]>(
+				`/api/volunteers/search/?keyword=${query}`,
+			);
+			setVolunteerList(responseData);
+		};
+		fetchData();
+	}, [query]);
+
+	// 커뮤니티 조회
+	useEffect(() => {
+		const fetchData = async () => {
+			const responseData = await get<communityListType[]>(
+				`/commuities/search/?keyword=${query}`,
+			);
+			setCommunityList(responseData);
+		};
+		fetchData();
+	}, [query]);
+
+	const validSearch =
+		query !== '' && volunteerList.length !== 0 && communityList.length !== 0;
+
 	return (
 		<SearchSection>
-			<SearchContainer>
-				<SearchLogo src={searchLogo} alt='search' />
-				<SearchInput placeholder='검색어를 입력하세요' />
-			</SearchContainer>
-			<VolunteerContainer>
-			</VolunteerContainer>
-			<CommunityContainer>
-			</CommunityContainer>
+			<SearchBar onSearch={handleSearch} />
+			{validSearch ? (
+				<>
+					<VolunteerTitle>봉사활동 검색결과</VolunteerTitle>
+					<VolunteerContainer>
+						{volunteerList.slice(0, 8).map((item) => (
+							<VolunteerCard key={item._id} data={item} />
+						))}
+					</VolunteerContainer>
+					<CommunityTitle>커뮤니티 검색결과</CommunityTitle>
+					<CommunityContainer>
+						{communityList.slice(0, 6).map((item) => (
+							<CommunityCard key={item._id} data={item} />
+						))}
+					</CommunityContainer>
+				</>
+			) : (
+				<NoSearchContainer>
+					<NoKeyword>검색결과가 없습니다.</NoKeyword>
+					<VolunteerTitle>아래와 같은 키워드는 어떠신가요?</VolunteerTitle>
+					<KeywordContainer>
+						{keywordData.map((item, index) => (
+							<KeywordBox key={index}>{item}</KeywordBox>
+						))}
+					</KeywordContainer>
+				</NoSearchContainer>
+			)}
 		</SearchSection>
-	); 
+	);
 };
 
 export default Search;
+
+const keywordData = [
+	'어린이',
+	'어른',
+	'유기견',
+	'보육원',
+	'장애인',
+	'교육',
+	'다문화',
+];
--- src/pages/searchPage/style.ts
+++ src/pages/searchPage/style.ts
@@ -1,37 +1,67 @@
 import styled from 'styled-components';
 
 export const SearchSection = styled.div`
-  position: relative;
-  margin: 0;
-  padding: 0;
+	position: relative;
+	margin: 0;
+	padding: 0;
 `;
 
-export const SearchContainer = styled.div`
-  margin: 6rem 60rem;
-  padding: 1rem;
-  min-width: 72rem;
-  display: flex;
-  border: 2px solid #CCCCCC;;
-  border-radius: 12px;
-`;
-
-export const SearchLogo = styled.img`
-  width: 5%;
-  cursor: pointer;
-`;
-
-export const SearchInput = styled.input`
-  width: 92%;
-  margin-left: 3%;
-  border: none;
-  outline: none;
-  font-size: 2rem;
+export const VolunteerTitle = styled.h2`
+	margin: 12rem 40rem 0;
+	width: 50rem;
+	font-size: 3rem;
+	font-weight: 900;
 `;
 
 export const VolunteerContainer = styled.div`
-  min-height: 40rem;
+	min-width: 112rem;
+	margin: 4rem 40rem;
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+`;
+
+export const CommunityTitle = styled.h2`
+	margin: 6rem 40rem 4rem;
+	width: 30rem;
+	font-size: 3rem;
+	font-weight: 900;
 `;
 
 export const CommunityContainer = styled.div`
-  min-height: 40rem;
+	margin: 0 40rem;
+	padding-bottom: 12rem;
+	min-width: 112rem;
+	height: 89rem;
+	display: grid;
+	grid-template-rows: repeat(6, 1fr);
+`;
+
+export const NoSearchContainer = styled.div`
+	min-height: 100rem;
+`;
+
+export const NoKeyword = styled.h2`
+	margin: 20rem 0;
+	text-align: center;
+	font-size: 3rem;
+	font-weight: 900;
+	color: #888888;
+`;
+
+export const KeywordContainer = styled.div`
+	margin: 12rem 40rem;
+	min-width: 112rem;
+	display: grid;
+	grid-template-columns: repeat(6, 1fr);
+	column-gap: 3rem;
+	row-gap: 5rem;
+`;
+
+export const KeywordBox = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	aspect-ratio: 1;
+	border-radius: 50%;
+	border: 2px solid black;
 `;
