149ac01fc8d09a8fb689b43e62fdf2da0f1a70eb
Yoonjae Choi <dbswo9795@gmail.com>
2023-06-06 10:03:11 +0900

    [Design] Header 카테고리 디자인 추가 카테고리 클릭 시 색깔 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -19,6 +19,7 @@ import searchLogo from '../../assets/icons/search.svg';
 
 const Header = () => {
 	const [checkToken, setCheckToken] = useState<boolean>(false);
+	const [click, setClick] = useState<string>('home');
 	const navigate = useNavigate();
 
 	// 토큰 유무
@@ -31,23 +32,27 @@ const Header = () => {
 	// 로그인 버튼을 클릭하여 로그인 화면으로 이동
 	const loginHandler = () => {
 		navigate('/login');
+		setClick(() => 'home');
 	};
 
 	// 로그아웃 버튼 클릭하여 토큰 삭제
 	const logoutHandler = () => {
 		deleteToken();
+		setClick(() => 'home');
 		navigate('/');
 		window.location.reload();
 	};
 
 	// 로고를 클릭하여 메인 페이지로 이동
 	const mainHandler = () => {
+		setClick(() => 'home');
 		navigate('/');
 	};
 
 	// search 로고를 클릭하여 검색 화면으로 이동
 	const searchHandler = () => {
 		navigate('/search');
+		setClick(() => 'home');
 	};
 
 	return (
@@ -58,8 +63,22 @@ const Header = () => {
 				</LogoContainer>
 				<NavContainer>
 					<NavCategory to='/'>봉사모집하기</NavCategory>
-					<NavCategory to='/community/findfriend'>커뮤니티</NavCategory>
-					<NavCategory to='/review'>봉사후기</NavCategory>
+					<NavCategory
+						to='/community/findfriend'
+						className={click === 'community' ? 'community' : ''}
+						onClick={() => {
+							setClick(() => 'community');
+						}}>
+						커뮤니티
+					</NavCategory>
+					<NavCategory
+						to='/review'
+						className={click === 'review' ? 'review' : ''}
+						onClick={() => {
+							setClick(() => 'review');
+						}}>
+						봉사후기
+					</NavCategory>
 				</NavContainer>
 				<UtilContainer>
 					{!checkToken ? (
