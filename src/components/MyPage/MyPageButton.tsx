0ef525dbe338e6335ffb2d4c9524492908fb7620
Yoonjae Choi <dbswo9795@gmail.com>
2023-06-07 18:27:27 +0900

    [Design] myPage 버튼 이미지 삭제

--- src/components/MyPage/MyPageButton.tsx
+++ src/components/MyPage/MyPageButton.tsx
@@ -1,23 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import useAuthStore from '@src/store/useAuthStore.ts';
 import { getToken } from '@api/Token';
-import { ButtonContainer, Image } from './myPage.ts';
-
-import myPic from '@assets/images/car.png';
+import { ButtonContainer, ButtonWord } from './myPage.ts';
 
 const MyPageButton = () => {
-	const { userData, initialize } = useAuthStore();
-
-	useEffect(() => {
-		initialize();
-	}, []);
-
 	return (
 		<>
 			{getToken() !== null ? (
 				<ButtonContainer to='/mypage'>
-					<Image src={myPic} />
+					<ButtonWord>마이페이지</ButtonWord>
 				</ButtonContainer>
 			) : (
 				''
