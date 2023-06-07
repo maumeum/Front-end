01583befa91da0dc11e393fd75ee82921bbc6332
Yoonjae Choi <dbswo9795@gmail.com>
2023-06-07 15:06:08 +0900

    [Fix] 이메일, 비밀번호 오류 시 alert창 뜨도록 구현

--- src/pages/userPage/login.tsx
+++ src/pages/userPage/login.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 import { post } from '@api/Api';
 import { setToken } from '@api/Token';
@@ -45,19 +46,28 @@ const Login = () => {
 		}
 		setCheckData(true);
 
-		const response: { token: string } = await post('/api/login', {
-			email,
-			password,
-		});
+		try {
+			const response: { token: string } = await post('/api/login', {
+				email,
+				password,
+			});
 
-		// 토큰이 있다면 localStorage에 토큰 저장
-		if (response.token) {
-			setToken(response.token);
+			// 토큰이 있다면 localStorage에 토큰 저장
+			if (response.token) {
+				setToken(response.token);
+			}
+
+			setCheckData(true);
+			navigate('/');
+			window.location.reload();
+		} catch (err) {
+			Swal.fire({
+				icon: 'error',
+				title: '이메일 또는 비밀번호가 일치하지 않습니다.',
+				confirmButtonColor: '#d33',
+			});
+			window.location.reload();
 		}
-
-		setCheckData(true);
-		navigate('/');
-		window.location.reload();
 	};
 
 	return (
