64ca8f242a4bddcc8ee2c3b9b9d36ac54120bc10
Yoonjae Choi <dbswo9795@gmail.com>
2023-06-10 13:02:48 +0900

    [refactor] 함수 로직 수정

--- src/utils/signUpCheck.ts
+++ src/utils/signUpCheck.ts
@@ -1,18 +1,14 @@
 // 유효성 검사 함수
-export const validEmail = (email: string): boolean | null => {
+export const validEmail = (email: string): boolean => {
 	return (
-		email.match(/^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/) &&
-		email !== ''
+		email.match(/^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/) !== null
 	);
 };
 
-export const validPassword = (password: string): boolean | undefined => {
-	if (password.length < 4 || password.length > 20) {
-		return false;
-	}
-	return true;
+export const validPassword = (password: string): boolean => {
+	return password.length >= 4 && password.length <= 20;
 };
 
-export const validPhoneNum = (phoneNum: string): boolean | null => {
-	return phoneNum.match(/^\d+$/) && phoneNum !== '';
+export const validPhoneNum = (phoneNum: string): boolean => {
+	return phoneNum.match(/^\d+$/) !== null;
 };
