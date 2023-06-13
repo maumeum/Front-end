3494f90b7f02e76e7cd57fd8e757e88300a11710
Yoonjae Choi <dbswo9795@gmail.com>
2023-06-13 16:43:58 +0900

    [Feat] 단체 인증 모달 구현

---
+++ src/components/Modal/TeamModal.tsx
@@ -0,0 +1,119 @@
+import ReactModal from 'react-modal';
+import Swal from 'sweetalert2';
+import dayjs from 'dayjs';
+
+import { post } from '@api/api';
+import {
+	TeamButtonContainer,
+	AcceptButton,
+	RefuseButton,
+} from '@components/Card/card.ts';
+import alertData from '@utils/swalObject';
+import {
+	customStyles,
+	MainContainer,
+	TopContainer,
+	MainTitle,
+	Title,
+	Desc,
+	TeamLogo,
+	DescContainer,
+	RemainContainer,
+	RemainDesc,
+} from '@components/Modal/modal';
+import { TeamType } from '@src/types/cardType';
+import DataType from '@src/types/dataType';
+
+interface TeamModalProps {
+	isOpen: boolean;
+	closeModal: () => void;
+	teamData: TeamType;
+}
+
+const TeamModal = ({ isOpen, closeModal, teamData }: TeamModalProps) => {
+	const handleClose = () => {
+		closeModal();
+	};
+
+	const image = `http://localhost:5002/${teamData.image}`;
+	const date = dayjs(teamData.establishmentDate)
+		.locale('ko')
+		.format('YYYY년 MM월 DD일');
+	const introduction = teamData.introduction.split('\r\n');
+	const briefHistory = teamData.briefHistory.split('\r\n');
+
+	// 수락
+	const acceptHandler = () => {
+		Swal.fire(alertData.acceptTeamCard).then((result) => {
+			if (result.isConfirmed) {
+				post<DataType>(
+					'/api/team/auth/admin?status=true',
+					{
+						teamAuth_id: teamData._id,
+					},
+					{},
+				);
+				Swal.fire('수락되었습니다.');
+			}
+		});
+	};
+
+	// 거부
+	const refuseHandler = () => {
+		Swal.fire(alertData.refuseTeamCard).then((result) => {
+			if (result.isConfirmed) {
+				post<DataType>(
+					'/api/team/auth/admin?status=false',
+					{
+						teamAuth_id: teamData._id,
+					},
+					{},
+				);
+				Swal.fire('거부되었습니다.');
+			}
+		});
+	};
+
+	return (
+		<ReactModal
+			isOpen={isOpen}
+			onRequestClose={handleClose}
+			style={customStyles}>
+			<MainContainer>
+				<MainTitle>
+					[{teamData.category}] {teamData.teamName}
+				</MainTitle>
+				<TopContainer>
+					<TeamLogo src={image} alt='teamLogo' />
+					<DescContainer>
+						<Desc>신청자</Desc> {teamData.user_id.nickname}
+						<Desc>설립일</Desc> {date}
+						<Desc>소재지 주소</Desc> {teamData.location}
+						<Desc>전화번호</Desc> {teamData.phone}
+					</DescContainer>
+				</TopContainer>
+				<RemainContainer>
+					<Title>팀 소개</Title>
+					<RemainDesc>
+						{introduction.map((item, index) => (
+							<p key={index}>{item}</p>
+						))}
+					</RemainDesc>
+				</RemainContainer>
+				<RemainContainer>
+					<Title>주요 활동과 목적</Title>
+					<RemainDesc>
+						{briefHistory.map((item, index) => (
+							<p key={index}>{item}</p>
+						))}
+					</RemainDesc>
+				</RemainContainer>
+			</MainContainer>
+			<TeamButtonContainer>
+				<AcceptButton onClick={acceptHandler}>수락</AcceptButton>
+				<RefuseButton onClick={refuseHandler}>거부</RefuseButton>
+			</TeamButtonContainer>
+		</ReactModal>
+	);
+};
+export default TeamModal;
