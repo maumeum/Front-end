54ac644f205b11c65acf42e456b6c0dc472f2782
Yoonjae Choi <dbswo9795@gmail.com>
2023-06-13 21:56:52 +0900

    [Feat] 관리자 유저 신고 승인, 거부 기능 구현

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,7 +1,17 @@
+import { useEffect, Dispatch, SetStateAction } from 'react';
+import Swal from 'sweetalert2';
+
+import { patch, del } from '@api/api';
+import useAuthStore from '@src/store/useAuthStore';
+import alertData from '@utils/swalObject';
 import {
+	PostContainer,
 	PostListContainer,
 	PostTitle,
+	PostBox,
 	PostContents,
+	PostButtons,
+	AcceptButtons,
 	PostListLine,
 } from './PostListStyle';
 
@@ -9,15 +19,80 @@ type PostListProps = {
 	postTitle: string;
 	postContents: string;
 	onClick: () => void;
+	postId?: string;
+	postType?: string;
+	volunteerId?: string;
+	setIsModified?: Dispatch<SetStateAction<boolean>>;
 };
-const PostList = ({ postTitle, postContents, onClick }: PostListProps) => {
+const PostList = ({
+	postTitle,
+	postContents,
+	onClick,
+	postId,
+	postType,
+	volunteerId,
+	setIsModified,
+}: PostListProps) => {
+	const { userData, getUserData } = useAuthStore();
+	useEffect(() => {
+		getUserData();
+	}, []);
+
+	const AcceptClick = () => {
+		Swal.fire(alertData.AcceptReported).then((result) => {
+			if (result.isConfirmed) {
+				if (postType) {
+					del(`/api/community/admins/reports/applications/${postId}`, {});
+					if (setIsModified) {
+						setIsModified(() => true);
+					}
+				} else if (volunteerId) {
+					del(`/api/review/admins/reports/applications/${postId}`, {});
+					if (setIsModified) {
+						setIsModified(() => true);
+					}
+				}
+				Swal.fire('승인되었습니다.');
+			}
+		});
+	};
+
+	const CancelClick = () => {
+		Swal.fire(alertData.cancelReported).then((result) => {
+			if (result.isConfirmed) {
+				if (postType) {
+					patch(`/api/community/admins/reports/cancellations/${postId}`, {});
+					if (setIsModified) {
+						setIsModified(() => true);
+					}
+				} else if (volunteerId) {
+					patch(`/api/review/admins/reports/cancellations/${postId}`, {});
+					if (setIsModified) {
+						setIsModified(() => true);
+					}
+				}
+				Swal.fire('취소되었습니다.');
+			}
+		});
+	};
+
 	return (
 		<>
 			<PostListLine />
-			<PostListContainer onClick={onClick}>
-				<PostTitle>{postTitle}</PostTitle>
-				<PostContents>{postContents}</PostContents>
-			</PostListContainer>
+			<PostContainer>
+				<PostListContainer onClick={onClick}>
+					<PostTitle>{postTitle}</PostTitle>
+					<PostBox>
+						<PostContents>{postContents}</PostContents>
+					</PostBox>
+				</PostListContainer>
+				{userData !== null && userData.role === 'admin' && (
+					<>
+						<AcceptButtons onClick={AcceptClick}>수락</AcceptButtons>
+						<PostButtons onClick={CancelClick}>취소</PostButtons>
+					</>
+				)}
+			</PostContainer>
 		</>
 	);
 };
