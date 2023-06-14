fb4c059b1e4c3eb8757c7ac3d5d07c359758a18a
{Marina <{akflsk6040@gmail.com}>
2023-06-14 21:47:05 +0900

    [feat] 봉사활동 글쓰기 페이지 상태 추가

--- src/components/WritePage/VolunteerWritePage.tsx
+++ src/components/WritePage/VolunteerWritePage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
+import WritePageProps from './WritePageProps';
 import {
 	TextContainer,
 	Container,
@@ -9,23 +10,56 @@ import {
 	ButtonContainer,
 	ContentInput,
 	TextLength,
-} from '../WritePage/WritePageStyle';
-import Calendar from '@components/Calendar/Calendar';
+	LayoutContainer,
+	LayoutChildContainer,
+} from '@components/WritePage/WritePageStyle';
+import VolunteerCalendar from '@components/Calendar/VolunteerCalendar';
+import Selector from '@components/Selector/Selector.tsx';
+import UploadThumbnail from './UploadThumbnail';
+import actTypes from '@src/types/actTypeConstants';
 
-type WritePageProps = {
-	onSave: (inputTitle: string, textContent: string) => void;
-	onCancel: () => void;
-};
+interface VolunteerWritePageProps extends Omit<WritePageProps, 'onSave'> {
+	onSave: (
+		inputTitle: string,
+		textContent: string,
+		selectedActType: string,
+		thumbnail: File | null,
+		image: File | null,
+		inputRegisterCount: number,
+		deadline: Date,
+		startDate: Date,
+		endDate: Date,
+	) => void;
+}
 
-const WritePage = ({ onSave, onCancel }: WritePageProps) => {
+const VolunteerWritePage = ({ onSave, onCancel }: VolunteerWritePageProps) => {
 	const [content, setContent] = useState('');
 	const [inputTitle, setInputTitle] = useState('');
+	const [selectedActType, setSelectedActType] = useState('');
+	const [thumbnail, setThumbnail] = useState<File | null>(null);
+	const [image, setImage] = useState<File | null>(null);
+	const [inputRegisterCount, setInputRegisterCount] = useState<number>(0);
+	const [deadline, setDeadline] = useState<Date>(new Date());
 	const [startDate, setStartDate] = useState<Date>(new Date());
 	const [endDate, setEndDate] = useState<Date>(new Date());
 
+	// 만약 StartDate가 deadline보다 작다면 유저에게 경고창을 띄고, 다시 작성하게 한다.
+	// 인증된 유저만 봉사활동 글 등록할 수 있도록 로직 추가했읍니다!!!!!!!!!!!!!!
+	// user 필드에서 authorization이 true인 경우만 글 작성됩니다 아니면 에러를 띄워줍니다요.
+
 	const onClickHandler = () => {
-		onSave(inputTitle, content);
-		clearContent();
+		onSave(
+			inputTitle,
+			content,
+			selectedActType,
+			thumbnail,
+			image,
+			inputRegisterCount,
+			deadline,
+			startDate,
+			endDate,
+		);
+		clearContentData();
 	};
 
 	const handelInputContent = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
@@ -37,64 +71,98 @@ const WritePage = ({ onSave, onCancel }: WritePageProps) => {
 
 	const handleInputTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const text = e.target.value;
-		setInputTitle(text);
+		if (text.length <= 40) {
+			setInputTitle(text);
+		}
+	};
+
+	const handleInputCategory = (selectedValue: string) => {
+		setSelectedActType(selectedValue);
+	};
+
+	const handleInputRegisterCount = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const count = Number(e.target.value);
+		setInputRegisterCount(count);
 	};
 
 	const deleteContent = () => {
-		clearContent();
+		clearContentData();
 		onCancel();
 	};
 
-	const clearContent = () => {
+	const clearContentData = () => {
 		setInputTitle('');
 		setContent('');
+		setSelectedActType('');
+		setInputRegisterCount(0);
+		setDeadline(new Date());
+		setStartDate(new Date());
+		setEndDate(new Date());
+		setThumbnail(null);
+		setImage(null);
 	};
 
 	return (
 		<>
 			<Container>
 				<TitleInput
-					placeholder='모집하려는 봉사활동을 잘 나타낼 수 있는 제목을 지어주세요.'
+					placeholder='모집하려는 봉사활동을 잘 나타낼 수 있는 제목을 지어주세요. (40자 이하)'
 					value={inputTitle}
 					onChange={handleInputTitle}
 				/>
-				<TitleInput
-					placeholder='어떤 종류의 봉사활동인지 입력해주세요.'
-					value={inputTitle}
-					onChange={handleInputTitle}
-				/>
-				<TitleInput
-					placeholder='모집인원을 입력해주세요. (단위: 명)'
-					value={inputTitle}
-					onChange={handleInputTitle}
-				/>
-				<TitleInput
-					placeholder='모집 마감일을 선택해주세요.'
-					value={inputTitle}
-					onChange={handleInputTitle}
-				/>
-				<div
-					style={{
-						display: 'flex',
-						justifyContent: 'space-between',
-					}}>
-					<div
-						style={{
-							display: 'flex',
-							flexDirection: 'column',
-						}}>
-						<p>활동 시작일</p>
-						<Calendar selectedDate={startDate} setSelectedDate={setStartDate} />
-					</div>
-					<div
-						style={{
-							display: 'flex',
-							flexDirection: 'column',
-						}}>
-						<p>활동 종료일</p>
-						<Calendar selectedDate={endDate} setSelectedDate={setEndDate} />
-					</div>
+				<div>
+					<h2>카테고리</h2>
+					<Selector
+						value={selectedActType}
+						onChange={handleInputCategory}
+						options={[
+							{ value: actTypes.OLD, label: '노인' },
+							{ value: actTypes.FOOD, label: '급식' },
+							{ value: actTypes.ECO, label: '환경' },
+							{ value: actTypes.ANIMAL, label: '동물' },
+							{ value: actTypes.DISABLE, label: '장애인' },
+							{ value: actTypes.SUPPORT, label: '생활편의지원' },
+							{ value: actTypes.MEDICAL, label: '의료' },
+							{ value: actTypes.EDU, label: '교육' },
+						]}
+					/>
 				</div>
+				<div>
+					<h2>썸네일 등록</h2>
+					<UploadThumbnail setFile={setThumbnail} imageType='thumbnail' />
+				</div>
+				<div>
+					<h2>이미지 등록</h2>
+					<UploadThumbnail setFile={setImage} imageType='image' />
+				</div>
+				<TitleInput
+					placeholder='모집인원을 입력해주세요. (숫자만 입력해주세요.)'
+					value={inputRegisterCount === 0 ? '' : inputRegisterCount}
+					onChange={handleInputRegisterCount}
+				/>
+				<LayoutContainer>
+					<LayoutChildContainer>
+						<h2>모집 마감일</h2>
+						<VolunteerCalendar
+							selectedDate={deadline}
+							setSelectedDate={setDeadline}
+						/>
+					</LayoutChildContainer>
+					<LayoutChildContainer>
+						<h2>활동 시작일</h2>
+						<VolunteerCalendar
+							selectedDate={startDate}
+							setSelectedDate={setStartDate}
+						/>
+					</LayoutChildContainer>
+					<LayoutChildContainer>
+						<h2>활동 종료일</h2>
+						<VolunteerCalendar
+							selectedDate={endDate}
+							setSelectedDate={setEndDate}
+						/>
+					</LayoutChildContainer>
+				</LayoutContainer>
 				<TextContainer>
 					<ContentInput
 						placeholder='봉사활동 주제와 일정을 포함하여 내용을 작성해주세요. 썸네일용 이미지는 필수입니다.'
@@ -113,4 +181,4 @@ const WritePage = ({ onSave, onCancel }: WritePageProps) => {
 	);
 };
 
-export default WritePage;
+export default VolunteerWritePage;
---
+++ src/types/actTypeConstants.ts
@@ -0,0 +1,12 @@
+enum actTypes {
+	OLD = '노인',
+	FOOD = '급식',
+	ECO = '환경',
+	ANIMAL = '동물',
+	DISABLE = '장애인',
+	SUPPORT = '생활편의지원',
+	MEDICAL = '의료',
+	EDU = '교육',
+}
+
+export default actTypes;
