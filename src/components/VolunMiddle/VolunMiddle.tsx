b312dca33fcee679ac592d77db59f0116b97aa5f
Yoonjae Choi <dbswo9795@gmail.com>
2023-06-16 16:15:56 +0900

    [Chore] build error 수정

--- src/components/VolunMiddle/VolunMiddle.tsx
+++ src/components/VolunMiddle/VolunMiddle.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import CommentSection from '../Comment/Comment';
 import ActivityIntro from './ActivityIntro';
 import IntroTeam from './IntroTeam';
