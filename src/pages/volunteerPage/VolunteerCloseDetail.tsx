0fc9480d8f57d712001cc560208639adb2146de0
Yoonjae Choi <dbswo9795@gmail.com>
2023-06-17 10:47:00 +0900

    [Feat] 종료된 봉사 상세 페이지 구현

---
+++ src/pages/volunteerPage/VolunteerCloseDetail.tsx
@@ -0,0 +1,13 @@
+import VolunMiddle from '@src/components/VolunMiddle/VolunMiddle';
+import VolunInfo from '@components/VolunIntro/VolunInfo.tsx';
+
+const VolunDetailPage = () => {
+	return (
+		<>
+			<VolunInfo />
+			<VolunMiddle />
+		</>
+	);
+};
+
+export default VolunDetailPage;
